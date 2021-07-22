6c47e1039f0e3c7696314ad9a609cef141475a6f
nkarl <nordenk@pm.me>
2021-07-22 16:30:42 -0700

    finished stage-2

--- Web Scraper/task/scraper.py
+++ Web Scraper/task/scraper.py
@@ -1,12 +1,32 @@
 """Module: Web Scraper"""
-import json
 import requests
+from bs4 import BeautifulSoup
+import re
+import functools
 
 
-url = input()
-r = requests.get(url)
-result = r.json()
-try:
-    print(result['content'])
-except KeyError:
-    print("invalid quote resource")
+def scrapeIt():
+    # default language settings for requests
+    default_lang = {'Accept-Language': 'en-US,en;q=0.5'}
+
+    url = input()
+    if "title" not in url:
+        print("Invalid movie page!")
+    else:
+        try:
+            r = requests.get(url, headers=default_lang)   # make request
+            soup = BeautifulSoup(r.content, "html.parser")
+            title = soup.find("title").text.split(' (')[0]
+            description = soup.find("meta", property=re.compile("description")).attrs['content'].split('. ')[2:]
+            # description = description.join(". ")
+            description = functools.reduce(lambda a, b: a + '. ' + b, description)
+            print(description)
+            if title and description is not None:
+                output = {'title': title, 'description': description}
+                print(output)
+        except TypeError:
+            print("Invalid movie page!")
+
+
+if __name__ == "__main__":
+    scrapeIt()
