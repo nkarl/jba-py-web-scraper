689aa5f8c998e1e1f4edf9989a21451c99a05081
Karl N <khoa.norden@gmail.com>
2021-07-23 09:20:29 -0700

    updated

--- Web Scraper/task/engine/core.py
+++ Web Scraper/task/engine/core.py
@@ -1,16 +1,16 @@
-import _io
-
+import bs4.element
 import requests
 import functools
+import string
 import re
-import os.path
 
 from bs4 import BeautifulSoup
+from typing import Final
 
-
-# default language setting
-DEFAULT_LANG = {'Accept-Language': 'en-US,en;q=0.5'}
-NATURE_SOURCE = "https://www.nature.com"
+# default language setting & the host to be scraped
+DEFAULT_LANG: Final = {'Accept-Language': 'en-US,en;q=0.5'}
+NATURE_SOURCE: Final = "https://www.nature.com"
+PUNCTUATION: Final = string.punctuation
 
 
 def request_response(url: str) -> requests.models.Response:
@@ -18,35 +18,45 @@ def request_response(url: str) -> requests.models.Response:
     if "20" not in str(r.status_code):
         print(f"The URL returned {r.status_code}")
     return r
-    # try:
-    #     if "20" not in str(r.status_code):
-    #         raise ValueError
-    # except ValueError:
-    #     print(f"The URL returned {r.status_code}")
-    # else:
-    #     return r
 
 
-def collect_links(r: requests.models.Response) -> [str]:
+# def collect_links(r: requests.models.Response) -> [str]:
+#     soup = BeautifulSoup(r.content, "html.parser")
+#     links = soup.find_all('article', href=re.compile("/articles/"), itemprop=re.compile("url"))
+#     articles = [NATURE_SOURCE + lnk.attrs['href'] for lnk in links]
+#     return articles
+
+
+def collect_articles(r: requests.models.Response) -> [str]:
     soup = BeautifulSoup(r.content, "html.parser")
-    links = soup.find_all('a', href=re.compile("/articles/"), itemprop=re.compile("url"))
-    articles = [NATURE_SOURCE + lnk.attrs['href'] for lnk in links]
-    return articles
+    articles = soup.find_all("article")
+    # links = [(item.find("a").text, item.find("a")["href"]) for item in articles if is_article(item)]
+    links = []
+    for item in articles:
+        if is_article(item):
+            a_tag = item.find("a")
+            links += [(a_tag.text, a_tag["href"])]
+    return links
 
 
-def transform_title(content: bytes) -> str:
-    soup = BeautifulSoup(content, "html.parser")
-    title = soup.find("title").text
-    delim = ['|', ':']
-    for d in delim:
-        if d in title:
-            title = '_'.join(title.split(d)[0].split(' '))
-    return title[0:-1] + '.txt'
+def is_article(article: bs4.element.Tag) -> bool:
+    article_spans = [item.get("data-test", False) for item in article.find_all("span")]
+    return "article.type" in article_spans
+
+
+# def transform_title(content: bytes) -> str:
+#     soup = BeautifulSoup(content, "html.parser")
+#     title = soup.find("title").text
+#     delim = ['|', ':']
+#     for d in delim:
+#         if d in title:
+#             title = '_'.join(title.split(d)[0].split(' '))
+#     return title[0:-1] + '.txt'
 
 
 def scrape_articles(entry: str):
     entry_r = request_response(entry)
-    links = collect_links(entry_r)
+    titles, links = collect_articles(entry_r)
     write_articles(links)
 
 
@@ -54,5 +64,5 @@ def write_articles(links: [str]):
     for link in links:
         content = request_response(link).content
         filename = transform_title(content)
-        with open(filename, "wb") as file:
+        with open(filename, "wb", encoding="utf-8") as file:
             file.write(content)
