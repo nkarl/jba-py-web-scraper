6f2081097ecb6b6481539cf573cb0b153866d93a
Karl N <khoa.norden@gmail.com>
2021-07-26 10:12:05 -0700

    updated

--- Web Scraper/task/scraper.py
+++ Web Scraper/task/scraper.py
@@ -1,11 +1,7 @@
 """Module: Web Scraper"""
-import requests
-from bs4 import BeautifulSoup
-import re
 import engine.core as core
 
 if __name__ == "__main__":
     fromEntry: str = "https://www.nature.com/nature/articles"
-    # core.scrape_articles(fromEntry)
     scraper = core.NatureNewsScraper()
     scraper.write_articles(fromEntry)
