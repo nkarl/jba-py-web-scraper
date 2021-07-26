48011f5ce8f611ca11fa8c15e3957ccaaee8f4f7
Karl N <khoa.norden@gmail.com>
2021-07-26 10:04:51 -0700

    finished stage-4

--- Web Scraper/task/engine/core.py
+++ Web Scraper/task/engine/core.py
@@ -1,5 +1,3 @@
-import re
-
 import bs4.element
 import requests
 import string
@@ -7,7 +5,6 @@ from furl import furl
 
 from bs4 import BeautifulSoup
 from typing import Final
-from hstest.check_result import CheckResult
 
 # default language setting & the host to be scraped
 DEFAULT_LANG: Final = {'Accept-Language': 'en-US,en;q=0.5'}
@@ -17,103 +14,72 @@ IN_CHARS: Final = PUNCTUATION + " "
 OUT_CHARS: Final = ''.join(['_' if c == ' ' else ' ' for c in IN_CHARS])
 
 
-def request_response(url: str) -> requests.models.Response:
-    r = requests.get(url, headers=DEFAULT_LANG)
-    if "20" not in str(r.status_code):
-        print(f"The URL returned {r.status_code}")
-    return r
+class NatureNewsScraper:
+    @staticmethod
+    def request_response(url: str) -> requests.models.Response:
+        r = requests.get(url, headers=DEFAULT_LANG)
+        if "20" not in str(r.status_code):
+            print(f"The URL returned {r.status_code}")
+        return r
 
+    @staticmethod
+    def tag_containing_view_article(tag: bs4.element) -> bool:
+        return tag.has_attr("data-track-action") and \
+               tag.get("data-track-action", None) == "view article"
 
-def collect_articles(r: requests.models.Response) -> [(str, str)]:
-    soup = BeautifulSoup(r.content, "html.parser")
-    articles = soup.find_all("article")
-    # links = [(item.find("a").text, item.find("a")["href"]) for item in articles if is_article(item)]
-    links = []
-    for item in articles:
-        if is_article(item):
-            a_tag = item.find("a")
-            links += [(a_tag.text, NATURE_SOURCE+ a_tag["href"])]
-    return links
+    @staticmethod
+    def tag_containing_article_type(tag: bs4.element) -> bool:
+        return tag.name == "span" and \
+               tag.has_attr("data-test") and \
+               tag.get("data-test", None) == "article.type"
 
-
-def is_article(article: bs4.element.Tag) -> bool:
-    article_spans = [item.get("data-test", False) for item in article.find_all("span")]
-    return "article.type" in article_spans
-
-
-def transform_title(title: str) -> str:
-    dictionary = title.maketrans(IN_CHARS, OUT_CHARS)
-    return ''.join(title.translate(dictionary).split(' '))
-
-
-def scrape_articles(entry: str):
-    # entry_r = request_response(entry)
-    # links = collect_articles(entry_r)
-    # articles = get_articles(links)
-    # for path, body in articles:
-    #     with open(path, "wb") as file:
-    #         file.write(body.encode("utf-8"))
-    scraper = NatureScraper()
-    contents = scraper.get_article_title_and_content(entry)
-    return contents
-
-
-
-def is_body(soup):
-    return soup.find("div", class_=re.compile("article-body")) is not None
-    # return body.contents if body is not None else None
-
-
-def get_articles(links: [(str, str)]):
-    articles = []
-    for title, link in links:
-    # for i in range(9):  # hardcoded to 4 articles
-    #     title, link = links[i]
-        path = transform_title(title) + ".txt"
-        content = request_response(link).content
-        soup = BeautifulSoup(content, "html.parser")
-        if is_body(soup):
-            articles += [(path, soup.find("div", class_=re.compile("article-body")))]
-    return articles
-
-
-class NatureScraper:
-    def tag_leading_to_view_article(self, tag):
-        return tag.has_attr("data-track-action") and tag["data-track-action"] == "view article"
-
-    def tag_containing_atricle_type(self, tag):
-        return tag.name == "span" and tag.has_attr("data-test") and tag.get("data-test", None) == "article.type"
-
-    def tag_containing_article_title(self, tag):
-        return tag.name == "h1" and ("article" in tag["class"][0] and "title" in tag["class"][0])
-
-    def tag_containing_article_body(self, tag):
-        return tag.name == "div" and ("article" in tag.get("class", [""])[0] and "body" in tag.get("class", [""])[0])
-
-    def get_article_links_of_type(self, url, article_type="News"):
-        origin_url = furl(url)
-        try:
-            articles_resp = requests.get(url)
-        except Exception:
-            return CheckResult.wrong("An error occurred when tests tried to connect to the Internet page.\n"
-                                     "Please, try again.")
-        soup = BeautifulSoup(articles_resp.text, "html.parser")
-        articles = soup.find_all(self.tag_containing_atricle_type)
-        articles = list(filter(lambda x: x.text.strip() == article_type, articles))
+    def collect_news_links(self, entry) -> [str]:
+        url_origin = furl(entry).origin
+        r = self.request_response(entry)
+        soup = BeautifulSoup(r.content, "html.parser")
+        articles = soup.find_all(self.tag_containing_article_type)
+        news_articles = list(filter(lambda x: x.text.strip() == "News", articles))
         return [
-            furl(origin_url).add(path=x.find_parent("article").find(self.tag_leading_to_view_article).get("href")).url \
-            for x in articles]
+            furl(url_origin).add(path=x.find_parent("article").find(self.tag_containing_view_article).get("href")).url \
+            for x in news_articles
+        ]
 
-    def get_article_title_and_content(self, url):
-        try:
-            article = requests.get(url)
-        except Exception:
-            return CheckResult.wrong("An error occurred when tests tried to connect to the Internet page.\n"
-                                     "Please, try again.")
-        soup = BeautifulSoup(article.text, "html.parser")
+    @staticmethod
+    def tag_containing_article_title(tag) -> bool:
+        return tag.name == "h1" and \
+               ("article" in tag["class"][0] and "title" in tag["class"][0])
+
+    @staticmethod
+    def tag_containing_article_body(tag) -> bool:
+        return tag.name == "div" and \
+               ("article" in tag.get("class", [""])[0] and "body" in tag.get("class", [""])[0])
+
+    def scrape_title_and_content(self, url) -> (str, str):
+        r = self.request_response(url)
+        soup = BeautifulSoup(r.content, "html.parser")
         title = soup.find(self.tag_containing_article_title)
         content = soup.find(self.tag_containing_article_body)
         if title and content:
             return title.text.strip(), content.text.strip()
         else:
             return title, content
+
+    def scrape_articles(self, links: [str]) -> [(str, str)]:
+        contents = []
+        for link in links:
+            contents += [(self.scrape_title_and_content(link))]
+        return contents
+
+    @staticmethod
+    def transform_title(title: str) -> str:
+        dictionary = title.maketrans(IN_CHARS, OUT_CHARS)
+        return ''.join(title.translate(dictionary).split(' '))
+
+    def write_articles(self, entry: str):
+        links = self.collect_news_links(entry)
+        contents = self.scrape_articles(links)
+        for url in links:
+            title, content = self.scrape_title_and_content(url)
+            path = self.transform_title(title)
+            with open(path + ".txt", "wb") as file:
+                file.write(content.encode(encoding="utf-8"))
--- Web Scraper/task/scraper.py
+++ Web Scraper/task/scraper.py
@@ -7,5 +7,5 @@ import engine.core as core
 if __name__ == "__main__":
     fromEntry: str = "https://www.nature.com/nature/articles"
     # core.scrape_articles(fromEntry)
-    scraper = core.NatureScraper()
-    article_links = scraper.get_article_links_of_type(fromEntry)
+    scraper = core.NatureNewsScraper()
+    scraper.write_articles(fromEntry)
